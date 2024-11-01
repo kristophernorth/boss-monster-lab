7e4fcc49a12bb6e52bf5269b66a26f9fa427c691
kristophernorth <kristopher.north@gmail.com>
2024-11-01 06:38:50 -0600

    commit

--- app.js
+++ app.js
@@ -1,5 +1,32 @@
 //#region state
-const
+const heroes = [
+  {
+    name: 'harriet',
+    type: 'hippo',
+    damage: 5,
+    health: 100,
+    maxHealth: 100
+  },
+  {
+    name: 'oslo',
+    type: 'monkey',
+    damage: 10,
+    health: 20,
+    maxHealth: 50
+  },
+]
+
+const boss = {
+  health: 100,
+  maxHealth: 100,
+  damage: 5,
+  level: 1
+}
+
+const potion = {
+  price: 10,
+  recoveryPoints
+}
 
 //#endregion
 
